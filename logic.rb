48a1c1a6c654d8be7c8774561dd74fe3c0093b35
Jimno <menaoir.jimmy@gmail.com>
2023-02-20 09:10:42 +0100

    two spaces indentation

--- logic.rb
+++ logic.rb
@@ -1,22 +1,20 @@
 def stock_picker(prices)
+  
+  profit_counter = 0
+  best_buy_sell_days = ""
+  
+  prices.each_with_index do |element1, index1|
+    prices.each_with_index do |element2, index2|
 
-    profit_counter = 0
-    best_buy_sell_days = ""
-    
-    prices.each_with_index do |element1, index1|
-        prices.each_with_index do |element2, index2|
+      profit = element2 - element1 
 
-            profit = element2 - element1 
-
-            if  profit > profit_counter && index1 < index2 # (2nd part ensures the price never goes any higher)
-                profit_counter = profit # => 12 in that case
-                best_buy_sell_days = [index1, index2]
-            end
-        end
+      if  profit > profit_counter && index1 < index2 # (2nd part ensures the price never goes any higher)
+        profit_counter = profit # => 12 in that case
+        best_buy_sell_days = [index1, index2]
+      end
     end
-
-    p best_buy_sell_days
-    best_buy_sell_days
-
+  end
+  best_buy_sell_days
 end
-stock_picker([17,3,6,9,15,8,6,1,10])
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
